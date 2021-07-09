84ed3cb968979685479afc13576d342a5beda0ae
ellysensei <diamantegabriel468@gmail.com>
2021-07-09 15:56:16 +0800

    blinking cursor update

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mononoki:pixelsize=16:antialias=true:autohint=true";
+static char *font = "hack:pixelsize=16:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
@@ -148,14 +148,20 @@ unsigned int defaultcs = 256;
 unsigned int defaultrcs = 257;
 unsigned int background = 258;
 
-/*
- * Default shape of cursor
- * 2: Block ("█")
- * 4: Underline ("_")
- * 6: Bar ("|")
- * 7: Snowman ("☃")
- */
-static unsigned int cursorshape = 6;
+/* https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Ps-SP-q.1D81
+* Default style of cursor
+* 0: Blinking block
+* 1: Blinking block (default)
+* 2: Steady block ("â–ˆ")
+* 3: Blinking underline
+* 4: Steady underline ("_")
+* 5: Blinking bar
+* 6: Steady bar ("|")
+* 7: Blinking st cursor
+* 8: Steady st cursor
+  */
+static unsigned int cursorstyle = 5;
+static Rune stcursor = 0x2603; /* snowman (U+2603) 
 
 /*
  * Default columns and rows numbers
