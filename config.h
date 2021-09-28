643ba73ef76c2fce7199892996d1cacbd2d3d858
Elly <diamantegabriel468@gmail.com>
2021-09-28 11:59:01 +0800

    font change, w3m

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mononoki nf:pixelsize=16:antialias=true:autohint=true";
+static char *font = "FiraCode NF:pixelsize=16:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
